7873f14023c38572a033ab89e6612c05f80a866d
Davyd Kuleba <36370603+glauuucoma@users.noreply.github.com>
2023-11-07 15:48:58 -0500

    Add files via upload

---
+++ SideMenuSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuSwiftUIApp.swift
+//
+//
+//  Created by Davyd Kuleba on 28/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTabbedView()
+        }
+    }
+}
